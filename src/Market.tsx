f9bc0d6ba5458ad467d420c70d82aafc726faa25
Ramon Medeiros <ramon.medeiros@voiapp.io>
2021-07-28 18:47:10 +0200

    Fix home

--- src/Market.tsx
+++ src/Market.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import Title from './template/Title';
 import CustomGrid from './template/CustomGrid';
-import { Link, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { Grid, Link, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
 import TrendingUpIcon from '@material-ui/icons/TrendingUp';
 import { ResponsiveContainer } from 'recharts';
@@ -16,21 +16,24 @@ export default function Market() {
         <CustomGrid>
             <Title>{params.market} market insights</Title>
             <ResponsiveContainer>
-                <div>
-                    <ListItem button component={Link} href={`/#/market/${params.market}/dividends`}>
-                        <ListItemIcon>
-                            <MonetizationOnIcon style={{ fontSize: 120 }} />
-                        </ListItemIcon>
-                        <ListItemText primary="Dividends" />
-                    </ListItem>
-
-                    <ListItem>
-                        <ListItemIcon>
-                            <TrendingUpIcon style={{ fontSize: 120 }} />
-                        </ListItemIcon>
-                        <ListItemText primary="Cheap Value Stocks" />
-                    </ListItem>
-                </div>
+                <Grid container >
+                    <Grid item xs>
+                        <ListItem button component={Link} href={`/#/market/${params.market}/dividends`}>
+                            <ListItemIcon>
+                                <MonetizationOnIcon style={{ fontSize: 120 }} />
+                            </ListItemIcon>
+                            <ListItemText primary="Dividends" />
+                        </ListItem>
+                    </Grid >
+                    <Grid item xs>
+                        <ListItem button component={Link} href={`/#/market/${params.market}/analysis`}>
+                            <ListItemIcon>
+                                <TrendingUpIcon style={{ fontSize: 120 }} />
+                            </ListItemIcon>
+                            <ListItemText primary="Cheap Value Stocks" />
+                        </ListItem>
+                    </Grid >
+                </Grid >
             </ResponsiveContainer>
         </CustomGrid>)
 }
