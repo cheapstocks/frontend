85064e975b1879a80b3cfc32261a9bd9dbe5df25
Ramon Medeiros <ramon.medeiros@voiapp.io>
2021-11-18 00:26:35 +0100

    Sort industries

--- src/MarketSectorAnalysis.tsx
+++ src/MarketSectorAnalysis.tsx
@@ -34,6 +34,7 @@ export default function MarketSectorAnalysis() {
           lotOfIndustries.push(info[i].category)
         }
       }
+      lotOfIndustries.sort()
       setIndustries(lotOfIndustries)
     })
 
